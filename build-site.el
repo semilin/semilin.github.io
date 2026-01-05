8e2381e9a9ceb80b2e33ee37f1f35efc08892ef4
semi <semilin@disroot.org>
2026-01-04 18:02:10 -0800

    fix

--- build-site.el
+++ build-site.el
@@ -18,12 +18,7 @@
       org-html-head-include-scripts nil	;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"/style.css\">
-<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Subscribe to (blog semi)\" href=\"https://semilin.dev/blog/feed.xml\" />
-<script>
-  if (window.location.pathname.endsWith(\'.html')) {
-    window.location.href = window.location.pathname.replace('.html', '');
-  }
-</script>"
+<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Subscribe to (blog semi)\" href=\"https://semilin.dev/blog/feed.xml\" />"
       org-html-preamble t
       org-html-preamble-format '(("en"
 				                          "<nav>semi
